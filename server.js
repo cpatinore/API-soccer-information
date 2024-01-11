4a1f4f127443838793b8f897a6a4934c23549972
Catherin Patiño Restrepo <cpatinore@unal.edu.co>
2024-01-11 18:24:02 -0500

    fix error

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
-
 app.listen(app.get("port"), async () => {
   console.log(
     `La aplicación está escuchando en http://localhost:${app.get("port")}`
