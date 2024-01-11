b414f7d6af0cff33ca9746ac9feb29ce79889f0d
Catherin Patiño Restrepo <cpatinore@unal.edu.co>
2024-01-11 18:53:42 -0500

    (fix route): Fix error in route team

--- controller/team.js
+++ controller/team.js
@@ -3,18 +3,18 @@ const { client } = require("../database/conect");
 async function getMainTeam(req, res) {
   try {
     const week = req.params.week;
+    console.log(req.params.players);
     const players = req.params.players ? req.params.players : 5;
-
     const search = await client.query(
       `SELECT MAX(training) FROM (
         SELECT COUNT(week) training FROM training WHERE week = $1 GROUP BY player
         ) training_info`,
       [week]
     );
-
+    console.log(search.rows);
     if (search.rows[0].max == 3) {
       let result = await client.query(
-        `SELECT 'Jugador' || player::integer AS Player, sum(power) * 0.2 + sum(speed) * 0.3 + sum(passes) * 0.5 as score 
+        `SELECT player AS Player, sum(power) * 0.2 + sum(speed) * 0.3 + sum(passes) * 0.5 as score 
         FROM training INNER JOIN player_performance as play ON performance = play.id WHERE week = $1 
         GROUP BY player ORDER BY score DESC LIMIT $2`,
         [week, players]
--- routes/team.js
+++ routes/team.js
@@ -2,6 +2,6 @@ const { Router } = require("express");
 const router = Router();
 const { getMainTeam } = require("../controller/team");
 
-router.get("/:week/:players?", getMainTeam);
+router.get('/:week/:players?', getMainTeam);
 
 module.exports = router;
